75046f36568a2204397d97fa13924e52343e5c3f
Sumit L <sumitlaishramjr123@gmail.com>
2025-06-23 13:57:52 +0530

    fix: token verify issue

--- internal/store/token.go
+++ internal/store/token.go
@@ -1,7 +1,9 @@
 package store
 
 import (
+	"crypto/sha256"
 	"database/sql"
+	"encoding/hex"
 	"time"
 )
 
@@ -10,9 +12,11 @@ type Token struct {
 }
 
 func (t *Token) Validate(token string) (bool, error) {
+	hash := sha256.Sum256([]byte(token))
+	hashToken := hex.EncodeToString(hash[:])
 	var exists bool
 	query := `SELECT EXISTS(SELECT 1 from user_invitations WHERE token = $1 AND expiry > $2)`
-	err := t.db.QueryRow(query, token, time.Now()).Scan(&exists)
+	err := t.db.QueryRow(query, hashToken, time.Now()).Scan(&exists)
 	if err != nil {
 		switch {
 		case err == sql.ErrNoRows:
